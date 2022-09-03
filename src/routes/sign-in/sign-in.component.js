16ca047b437a5b34070822e2481ef67b1c2156b5
enbarSoco <enbar2232@gmail.com>
2022-09-03 23:55:04 +0300

    Added sign-up form

--- src/routes/sign-in/sign-in.component.js
+++ src/routes/sign-in/sign-in.component.js
@@ -3,16 +3,19 @@ import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+import SignUpForm from "../../components/sign-up/sign-up.component";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>sign in with google</button>
+      <SignUpForm />
     </div>
   );
 };
