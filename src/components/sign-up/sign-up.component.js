4837b6709630013c4404e423b1513b129e524cff
enbarSoco <enbar2232@gmail.com>
2022-09-04 11:34:40 +0300

    created the form input component, button component

--- src/components/sign-up/sign-up.component.js
+++ src/components/sign-up/sign-up.component.js
@@ -4,7 +4,9 @@ import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
-
+import FormInput from "../form-input/form-input.component";
+import "./sign-up.styles.scss";
+import Button from "../button/button.component";
 const defaultFormFields = {
   displayName: "",
   email: "",
@@ -46,11 +48,12 @@ const SignUpForm = () => {
     }
   };
   return (
-    <div>
-      <h1>Sign up</h1>
+    <div className={"sign-up-container"}>
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
-        <label>Display Name</label>
-        <input
+        <FormInput
+          label={"Display Name"}
           type="text"
           required
           onChange={handleChange}
@@ -58,8 +61,8 @@ const SignUpForm = () => {
           value={displayName}
         />
 
-        <label>Display Name</label>
-        <input
+        <FormInput
+          label={"Email"}
           type="email"
           required
           onChange={handleChange}
@@ -67,25 +70,24 @@ const SignUpForm = () => {
           value={email}
         />
 
-        <label>Display Name</label>
-        <input
-          type="text"
+        <FormInput
+          label={"Password"}
+          type="password"
           required
           onChange={handleChange}
           name={"password"}
           value={password}
         />
 
-        <label>Display Name</label>
-        <input
-          type="text"
+        <FormInput
+          label={"Confirm Password"}
+          type="password"
           required
           onChange={handleChange}
           name={"confirmPassword"}
           value={confirmPassword}
         />
-
-        <button type={"submit"}>Sign Up</button>
+        <Button type={"submit"}>Sign Up</Button>
       </form>
     </div>
   );
