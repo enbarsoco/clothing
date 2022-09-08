1d4ded1ef44716379d5618eea0af3863d2db7c9c
enbarSoco <enbar2232@gmail.com>
2022-09-08 17:56:22 +0300

    replaced scss to styled-component

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -1,35 +1,37 @@
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { CartContext } from "../../contexts/cart.context";
+import { CartContext } from '../../contexts/cart.context';
 
-import Button from "../button/button.component";
-import CartItem from "../cart-item/cart-item.component";
+import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
-import "./cart-dropdwon.styles.scss";
+import {
+    CartDropdownContainer,
+    EmptyMessage,
+    CartItems,
+} from './cart-dropdown.styles';
 
 const CartDropdown = () => {
-  const { cartItems } = useContext(CartContext);
-  const navigate = useNavigate();
+    const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
 
-  const goToCheckoutHandler = () => {
-    navigate("/checkout");
-  };
+    const goToCheckoutHandler = () => {
+        navigate('/checkout');
+    };
 
-  return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
-        {cartItems.length ? (
-          cartItems.map((cartItem) => (
-            <CartItem key={cartItem.id} cartItem={cartItem} />
-          ))
-        ) : (
-          <span className="empty-message">Your cart is empty</span>
-        )}
-      </div>
-      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-    </div>
-  );
+    return (
+        <CartDropdownContainer>
+            <CartItems>
+                {cartItems.length ? (
+                    cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+                ) : (
+                    <EmptyMessage>Your cart is empty</EmptyMessage>
+                )}
+            </CartItems>
+            <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
+        </CartDropdownContainer>
+    );
 };
 
 export default CartDropdown;
